0991d8d01cb48abf4038d13298c25239b0908146
CodeXPranjal <pranjalwork749@gmail.com>
2026-08-12 10:41:50 +0530

    Solved Pattern 20

---
+++ Patterns/Pattern20.py
@@ -0,0 +1,28 @@
+class Solution:
+    def pattern20(self, n):
+
+        for i in range(1,n+1):
+
+            for j in range(i):
+                print("*",end="")
+
+            for j in range(2*(n-i),0,-1):
+                print(" ",end="")
+
+            for j in range(i):
+                print("*",end="")
+
+            print()
+
+        for i in range(1,n):
+
+            for j in range((n-i),0,-1):
+                print("*",end="")
+
+            for j in range((2*i),0,-1):
+                print(" ",end="")
+
+            for j in range((n-i),0,-1):
+                print("*",end="")
+
+            print()
