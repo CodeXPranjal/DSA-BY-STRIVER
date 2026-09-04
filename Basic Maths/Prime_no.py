a71c207f81b1b81d4fe639055c432c2026ed2b48
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-04 22:10:34 +0530

    Solved

--- Basic Maths/Prime_no.py
+++ Basic Maths/Prime_no.py
@@ -1,10 +1,11 @@
+import math
 class Solution:
     def isPrime(self, n):
         if n == 0 or n == 1:
             return False
         else:
-            for i in range(2,n):
-                if n%i==0:
+            for i in range(2, int(math.sqrt(n)) + 1):
+                if n % i == 0:
                     return False
                     
-            return True
+            return True      
