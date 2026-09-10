3dcb5656c370dfc86382a6bdc002b6764b065247
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-10 23:00:43 +0530

    Solved

---
+++ Basics of Recursion/Reverse_an_array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverse(self, arr: list, n: int) -> None:
+        left = 0
+        right = n - 1
+
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
