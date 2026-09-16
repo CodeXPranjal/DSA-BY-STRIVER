99445222fa5bb23b80d767a423a822f8c2c68ff7
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-17 01:05:05 +0530

    Solved

---
+++ Hashing/Dictionary/Highest_Occurring_Element_in_an_Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mostFrequentElement(self, nums):
+
+        freq = {}
+
+        for i in nums:
+            freq[i] = freq.get(i, 0) + 1
+
+        m = max(freq.values())
+
+        smallest = float('inf')
+
+        for i in freq:
+            if freq[i] == m:
+                smallest = min(smallest, i)
+
+        return smallest
