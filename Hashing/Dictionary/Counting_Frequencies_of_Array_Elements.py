5fb73bbdab620e489685ca9b12632593bafab3e1
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-16 23:12:38 +0530

    Solved

---
+++ Hashing/Dictionary/Counting_Frequencies_of_Array_Elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countFrequencies(self, nums):
+        freq = {}
+
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+
+        return list(freq.items())
