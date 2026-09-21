a4c20ad4453aed5324cbca821d2652489c230c1e
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-21 23:20:48 +0530

    Solved

---
+++ Basic Maths/Prime_no_in_a_range.py
@@ -0,0 +1,11 @@
+class Solution:
+    def primeUptoN(self, n):
+        count=0
+        for i in range(2,n+1):
+            for j in range(2,i):
+                if i%j==0:
+                    break
+                else:
+                    count+=1
+                    
+        return count
