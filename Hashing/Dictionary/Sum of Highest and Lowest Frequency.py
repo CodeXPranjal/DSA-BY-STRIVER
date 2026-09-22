5382f9cdfb8a99cc271dad28f1ecabcf888f2107
CodeXPranjal <pranjalwork749@gmail.com>
2026-09-22 22:40:54 +0530

    Solved

---
+++ Hashing/Dictionary/Sum of Highest and Lowest Frequency.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumHighestAndLowestFrequency(self, nums):
+        freq={}
+
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        
+        m=max(freq.values())+min(freq.values())
+
+        return m
